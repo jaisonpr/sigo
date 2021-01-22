469c28872adb605eb443fb9c7c55513e098cb7c6
Jaison PR <jaisonpr@gmail.com>
2021-01-22 10:43:51 -0300

    Contratos Mock

--- backend/gestao-processos/src/main/java/br/com/indtexbr/backend/gestaoprocessos/controller/ContratosController.java
+++ backend/gestao-processos/src/main/java/br/com/indtexbr/backend/gestaoprocessos/controller/ContratosController.java
@@ -2,6 +2,7 @@ package br.com.indtexbr.backend.gestaoprocessos.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,13 +18,16 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequestMapping("/gestao-processos/v1/contratos")
-public class Services {
+public class ContratosController {
+	
+	@Autowired ContratosClient contratosClient;
+	
 
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<ContratoDTO> getContratos() {
 		log.info("Services.getContratos");      
 		
-		GetContratoResponse response = new ContratosClient().getContrato( 1 ); 
+		GetContratoResponse response = contratosClient.getContrato( 1 ); 
 		
 		
 		ContratoDTO dto = new ContratoDTO();
@@ -31,6 +35,10 @@ public class Services {
 		dto.setId( response.getContrato().getId());
 		dto.setTexto( response.getContrato().getTexto());
 		dto.setArea( response.getContrato().getArea().name());
+		
+		System.out.println("---------------------");
+		System.out.println(dto);
+		System.out.println("---------------------");
 		
         return ResponseEntity.status(HttpStatus.OK).body(dto);
 	}
